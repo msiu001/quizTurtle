d2f5844775b70ccda31106992f48a36d4c58be50
Mario <msiu001@fiu.edu>
2017-12-19 15:07:00 -0500

    Finalised app, calculate results of the quiz, mark up of the results text and image, Go Back button with reset of the entire app (initialize variables etc...)

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -10,6 +10,62 @@
             var vm = this;
             vm.quizMetrics = quizMetrics;
             vm.DataService = DataService;
+            vm.getAnswerClass = getAnswerClass;
+            vm.setActiveQuestion = setActiveQuestion;
+            vm.calculatePercentage = calculatePercentage;
+            vm.reset = reset;
+            vm.activeQuestion = 0;
+            var scorePercentage = 0;
+
+
+            function getAnswerClass(index){
+                if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
+                    return "bg-success";
+                }
+                else if(index === DataService.quizQuestions[vm.activeQuestion].selected){
+                    return "bg-danger";
+                }
+            }
+
+
+            function setActiveQuestion(index) {
+                vm.activeQuestion =index;  
+            }
+
+
+            function calculatePercentage(){
+                var numberOfCorrectAnswers = 0;
+
+                for(var i =0; i< DataService.quizQuestions.length; i++){
+                    if(DataService.quizQuestions[i].correct === true){
+                        numberOfCorrectAnswers++;
+                    }
+                }
+
+                scorePercentage = numberOfCorrectAnswers / DataService.quizQuestions.length * 100;
+                return scorePercentage;
+
+
+                //or only scorePercentage = quizMetrics.numberOfCorrectAnswer / DataService.quizQuestions.length *100;
+            }
+
+
+            function reset(){
+                quizMetrics.changeState("results", false);
+                quizMetrics.changeState("quiz", false);
+                quizMetrics.numberOfCorrectAnswers =0;
+
+                for(var i= 0; i < DataService.quizQuestions.length ; i++){
+                    DataService.quizQuestions[i].selected = null;
+                    DataService.quizQuestions[i].correct = null;
+                }
+            }
+
 
         }
+
+
+         
+
+
 })();
